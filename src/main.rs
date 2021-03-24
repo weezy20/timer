32543c318c2af65404ed8c370da5ca7f7aeed8ce
Abhishek Shah <abhishekshah3@gmail.com>
2021-03-24 23:19:30 +0530

    added a input prompt

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ fn main() {
     std::io::stdin()
         .read_line(&mut inp)
         .expect("Failed to read");
-    match inp.parse::<u32>() {
+    match inp.trim().parse::<u32>() {
         Ok(time) => make_timer(time),
         Err(_) => {
-            println!("Cannot parse as u32");
+            eprintln!("Cannot parse as u32");
             process::exit(42);
         }
     }
 }
-fn make_timer(time: u32) {
-    let time = time * 60;
-    let five = Duration::new(time, 0);
-    assert_eq!(five.as_secs(), 5);
-}
+fn make_timer(time: u32) {}
+
+#[cfg(test)]
+mod tests {}
