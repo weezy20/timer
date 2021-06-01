decd618bcc3c4909aa55e90b2f08139807ca2939
abhishek <abhishekshah3@gmail.com>
2021-06-01 12:29:39 +0530

    Added a progress bar.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
-use std::process;
-use std::time::Duration;
+#![allow(unused, warnings)]
 
+use indicatif::ProgressBar;
+use std::{process, thread};
+
+use std::time::Duration;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Config {
+    /// Hours , default value 0
+    #[structopt(short, long = "hour", default_value = "0")]
+    hours: u32,
+
+    /// Minutes, default value 0
+    #[structopt(short, long = "min", default_value = "0")]
+    min: u32,
+
+    /// Seconds, default value 0
+    #[structopt(short, long = "secs", default_value = "0")]
+    seconds: u32,
+}
 fn main() {
-    println!("Enter the timer in minutes");
-    let mut inp = String::new();
-    std::io::stdin()
-        .read_line(&mut inp)
-        .expect("Failed to read");
-    match inp.trim().parse::<u32>() {
-        Ok(time) => make_timer(time),
-        Err(_) => {
-            eprintln!("Cannot parse as u32");
-            process::exit(42);
-        }
-    }
+    let cfg = Config::from_args();
+    let Config {
+        hours: hour,
+        min: min,
+        seconds: secs,
+    } = cfg;
+    // println!("hours : {}\nminutes : {}\nseconds: {}", hour, min, secs);
+    let time_in_seconds = hour * 3600 + min * 60 + secs;
+    make_timer(time_in_seconds);
 }
 fn make_timer(time: u32) {
-    let time = Duration::new((time * 60).into(), 0);
-    use std::thread;
-    thread::sleep(time);
-    println!("Times Up!");
+    let pb = ProgressBar::new(time.into());
+    pb.println("Timer started..");
+    for i in 1..=time {
+        thread::sleep(Duration::from_secs(1));
+        pb.inc(1);
+    }
+
+    pb.finish();
+    println!("Time's up!");
 }
